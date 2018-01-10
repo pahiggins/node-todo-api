a9d4b38cdd83fab565f43812333b84f8c4abcb53
Peter Higgins <pah@cenabo.com>
2018-01-10 12:38:08 +0000

    Add update script

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -6,7 +6,20 @@ MongoClient.connect('mongodb://localhost:27017/ToDoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
-  
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID("5a55eeb1e80de843cdb927cf")
+  }, {
+    $set: {
+      name: 'Mary'
+    },
+    $inc: {
+      age: 15
+    }
+  }, {
+    returnOriginal: false
+  }).then((res) => {
+    console.log(res);
+  });
 
   // db.close();
 });
